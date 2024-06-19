2ccff238dd4acfccb350da5e4fac27565064ac91
andikadp32 <andikadp32@gmail.com>
2024-06-19 17:02:51 +0700

    develop edit feature

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Home from './src/screens/home'
 import AddNote from './src/screens/addNote'
 import EditNote from './src/screens/editNote'
 
-const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage, addNote, deleteNote }) => {
+const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage, addNote, editNote, deleteNote }) => {
   switch (currentPage) {
     case 'home':
       return (
@@ -16,7 +16,13 @@ const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage, addNote, del
     case 'add':
       return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />
     case 'edit':
-      return <EditNote />
+      return (
+        <EditNote
+          noteList={noteList}
+          setCurrentPage={setCurrentPage}
+          editNote={editNote}
+        />
+      )
     default:
       return <Home />
   }
@@ -24,7 +30,6 @@ const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage, addNote, del
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState('home')
-
   const [noteList, setNoteList] = useState([
     {
       id: 1,
@@ -32,6 +37,8 @@ const App = () => {
       desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
     },
   ])
+
+  const [editNoteId, setEditNoteId] = useState(null)
 
   const addNote = (title, desc) => {
     const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1
@@ -46,6 +53,21 @@ const App = () => {
     ])
   }
 
+  const editNote = (id, title, desc) => {
+    const updatedNotes = noteList.map(note => {
+      if (note.id === id) {
+        return {
+          ...note,
+          title: title,
+          desc: desc,
+        };
+      }
+      return note
+    })
+
+    setNoteList(updatedNotes)
+  }
+
   const deleteNote = (id) => {
     setNoteList(noteList.filter((note) => note.id !== id));
   };
@@ -56,7 +78,9 @@ const App = () => {
       setCurrentPage={setCurrentPage}
       noteList={noteList}
       addNote={addNote}
+      editNote={editNote}
       deleteNote={deleteNote}
+      editNoteId={editNoteId}
     />
   )
 }
